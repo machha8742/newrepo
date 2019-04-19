2fab91f607616960963b60407d3f38f4045d1f1f
machha <machha@gmail.com>
2019-04-19 11:07:54 +0530

    new file

--- siri/src/siri/Webdriver_class.java
+++ siri/src/siri/Webdriver_class.java
@@ -2,29 +2,24 @@ package siri;
 
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-
+@Test
 public class Webdriver_class {
-
-	@Test
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 	
-		//Run yout First Selenium WebDriver script by copy paste below code after executing above five simple steps
-		 
-		 
-		class Webdriver_class
-		{
-		public static void main(String[] args)
-		{
-		WebDriver driver = new FirefoxDriver();
-		driver.get("http://google.com");
-		System.out.println(driver.getTitle());
-		driver.close();
-		}
-		}
-
+	
+	//Run yout First Selenium WebDriver script by copy paste below code after executing above five simple steps	 
+	
+	{
+		
+    System.setProperty("webdriver.chrome.driver", "C:\\dev files\\drivers\\chromedriver.exe");		
+	WebDriver driver =new ChromeDriver ();
+	driver.get("http://google.com");
+	System.out.println(driver.getTitle());
+	driver.close();
 	}
+	
 
+	
 }
